3dd68b85bd67e31d77b9f281636484ec45789617
tabliang <tabliang@tencent.com>
2021-10-22 17:10:27 +0800

    style: 完善代码格式

--- src/str/byteLength.ts
+++ src/str/byteLength.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-control-regex */
 /**
  * 获取字符串长度，一个中文算2个字符
  * @method str/byteLength
@@ -9,10 +10,12 @@
  */
 
 export function byteLength(str: string): number {
-  var aMatch;
+  let aMatch = null;
   if (!str) {
     return 0;
   }
   aMatch = str.match(/[^\x00-\xff]/g);
   return (str.length + (!aMatch ? 0 : aMatch.length));
 }
+
+export default byteLength;
