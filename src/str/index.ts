efbd85182a683918e90df82828bcaa3f7f61a763
tabliang <tabliang@tencent.com>
2021-11-10 14:39:15 +0800

    docs: 精简代码示例

--- src/str/index.ts
+++ src/str/index.ts
@@ -2,11 +2,6 @@
  * 字符串工具
  * @module spore-ui/tskit/src/str
  * @see https://github.com/SporeUI/spore-tskit/tree/master/src/str
- * @example
- * // 单独引入一个方法
- * import { setLock } from '@spore-ui/tskit'
- * import { setLock } from '@spore-ui/tskit/src/str'
- * import { setLock } from '@spore-ui/tskit/src/str/setLock'
  */
 
 export { byteLength } from './byteLength';
--- src/util/index.ts
+++ src/util/index.ts
@@ -3,11 +3,6 @@
  * 其他工具函数
  * @module spore-ui/tskit/src/util
  * @see https://github.com/SporeUI/spore-tskit/tree/master/src/util
- * @example
- * // 单独引入一个方法
- * import { abToHex } from '@spore-ui/tskit'
- * import { abToHex } from '@spore-ui/tskit/src/util'
- * import { abToHex } from '@spore-ui/tskit/src/util/abToHex'
  */
 
 export { abToHex } from './abToHex';
