cd0b16f6c55067f3ebcf90ec9e13cd86cb4afa3b
tabliang <tabliang@tencent.com>
2021-11-26 11:32:40 +0800

    chore: 完善 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,15 +9,23 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
-    project: 'tsconfig.eslint.json',
   },
+  extends: [
+    'airbnb-base',
+  ],
+  overrides: [{
+    files: ['*.ts', '*.tsx'],
+    extends: [
+      'airbnb-typescript/base',
+    ],
+    // @see https://stackoverflow.com/questions/58510287/parseroptions-project-has-been-set-for-typescript-eslint-parser/64488474
+    parserOptions: {
+      project: 'tsconfig.eslint.json',
+    },
+  }],
   plugins: [
     'jest',
     'no-for-of-loops',
     '@typescript-eslint',
-  ],
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
   ],
 };
