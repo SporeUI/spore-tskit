ded8877d8aaaae922a7126b9b5356038ac082506
TabSpace <pillar0514@163.com>
2022-06-09 14:20:43 +0800

    docs: ✏️ 完善 setPrepare 方法的注释与文档

--- src/fn/setPrepare.ts
+++ src/fn/setPrepare.ts
@@ -1,12 +1,15 @@
 /**
- * 包装为一个条件触发管理器
- * - 调用管理器的 ready 函数来激活条件。
- * - 之前插入管理器的函数按队列顺序执行。
- * - 之后插入管理器的函数立即执行。
- * - 返回的函数作用机制类似 jQuery.ready。
- * - 可以设置任何条件来触发 ready 状态。
+ * 以闭包机制构建一个条件触发管理器
+ * - 条件触发管理器本身是一个函数
+ *   - 条件触发管理器作用机制类似 jQuery.ready
+ *   - 传入 function 作为条件触发后的回调
+ *   - 可接收条件触发函数传递的 1 个参数
+ * - 调用条件触发管理器的 done 方法来激活回调条件
+ *   - 之前插入管理器的回调函数按队列顺序执行
+ *   - 之后插入管理器的回调函数立即执行
+ *   - done 方法可接收 1 个参数传给回调方法
  * @module setPrepare
- * @returns {Function} 条件触发管理器函数，传入一个 function 作为任务执行函数参数
+ * @returns {Function} 条件触发管理器函数
  * @example
  * import { setPrepare } from '@spore-ui/tskit';
  * // 生成一个管理器函数 timeReady
@@ -14,27 +17,27 @@
  *
  * // 设置条件为2秒后就绪
  * setTimeout(() => {
- *   timeReady.ready();
+ *   timeReady.done('ready');
  * }, 2000);
  *
  * // 调用管理器函数 timeReady，插入要执行的任务函数
- * timeReady(() => {
- *   // 2 秒后输出 1
- *   console.info(1);
+ * timeReady((str) => {
+ *   // 2 秒后输出 1 ready
+ *   console.info(1, str);
  * });
  *
  * // 调用管理器函数 timeReady，插入要执行的任务函数
- * timeReady(() => {
- *   // 2 秒后输出 2
- *   console.info(2);
+ * timeReady((str) => {
+ *   // 2 秒后输出 2 ready
+ *   console.info(2, str);
  * });
  *
  * // 2100ms 后执行
  * setTimeout(() => {
  *   // 调用管理器函数 timeReady，插入要执行的任务函数
- *   timeReady(() => {
- *     // 立即执行，输出 3
- *     console.info(3);
+ *   timeReady((str) => {
+ *     // 立即执行，输出 3 ready
+ *     console.info(3, str);
  *   });
  * }, 2100);
  */
